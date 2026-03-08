dd082ab0b99c1d61f250ecafd580b08399b24904
LimonTH <mel_ok_97@mail.ru>
2026-03-09 00:45:51 +0300

    layers patch

--- src/main/java/bodevelopment/client/blackout/util/render/RenderLayer.java
+++ src/main/java/bodevelopment/client/blackout/util/render/RenderLayer.java
@@ -5,10 +5,10 @@ public class RenderLayer {
     public static final float WORLD = 0.0F;
 
     /** Particles */
-    public static final float PARTICLES = -100.0F;
+    public static final float PARTICLES = 100.0F;
 
     /** ESP */
-    public static final float ESP = -200.0F;
+    public static final float ESP = 200.0F;
 
     /** Nametags */
     public static final float NAMETAGS = 300.0F;
@@ -26,15 +26,15 @@ public class RenderLayer {
     public static final float GUI_POPUP = 900.0F;
 
     /** Offset for sorting  */
-    public static final float OFFSET_LARGE = -10.0F;
+    public static final float OFFSET_LARGE = 10.0F;
     /** Offset for sorting */
-    public static final float OFFSET_SMALL = -1.0F;
+    public static final float OFFSET_SMALL = 1.0F;
     /** Offset for sorting */
-    public static final float OFFSET_MILI = -0.1F;
+    public static final float OFFSET_MILI = 0.1F;
     /** Offset for sorting */
-    public static final float OFFSET_MICRO = -0.01F;
+    public static final float OFFSET_MICRO = 0.01F;
     /** Offset for sorting */
-    public static final float OFFSET_NANO = -0.001F;
+    public static final float OFFSET_NANO = 0.001F;
 
     public static boolean isStandardLayer(float z) {
         return z == WORLD || z == ESP || z == PARTICLES || z == HUD || z == GUI ||
