0dad84224eed44eda8b50bff86f1a1c4fd722445
LimonTH <mel_ok_97@mail.ru>
2026-03-09 12:43:14 +0300

    .

--- src/main/java/bodevelopment/client/blackout/util/CompatUtils.java
+++ src/main/java/bodevelopment/client/blackout/util/CompatUtils.java
@@ -1,4 +1,22 @@
 package bodevelopment.client.blackout.util;
 
+import baritone.api.BaritoneAPI;
+import baritone.api.IBaritone;
+
 public class CompatUtils {
+    private static IBaritone baritone;
+
+    static {
+        try {
+            baritone = BaritoneAPI.getProvider().getPrimaryBaritone();
+        } catch (Throwable ignored) {}
+    }
+
+    public static boolean isBaritonePathing() {
+        try {
+            return baritone.getPathingBehavior().isPathing();
+        } catch (Throwable ignored) {
+            return false;
+        }
+    }
 }
