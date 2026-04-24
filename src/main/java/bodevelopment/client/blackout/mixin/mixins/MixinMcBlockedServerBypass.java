505e0da43e59021745cbacbab3c5736c69ea94aa
play-station451 <playstation451discord@gmail.com>
2026-04-23 20:27:14 -0400

    Create MixinMcBlockedServerBypass.java

---
+++ src/main/java/bodevelopment/client/blackout/mixin/mixins/MixinMcBlockedServerBypass.java
@@ -0,0 +1,19 @@
+package bodevelopment.client.blackout.mixin.mixins;
+
+import com.mojang.patchy.MojangBlockListSupplier;
+
+import java.util.function.Predicate;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(MojangBlockListSupplier.class)
+abstract class MixinMcBlockedServerBypass {
+
+	@Overwrite(remap = false)
+	public Predicate<String> createBlockList() {
+    return (address) -> address.equalsIgnoreCase("7b7t.net");
+    // :trol:
+		return null;
+	}
+}
